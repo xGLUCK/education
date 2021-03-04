63d88fa8f742db503c6cac193686e79eefe04aa3
xGLUCK <xGLUCK@users.noreply.github.com>
2021-03-04 22:42:37 +0300

    Add files via upload

---
+++ Практикум 13.8.3.py
@@ -0,0 +1,28 @@
+try:
+    AgeError = Exception
+    num_tickets = int(input('Какое количество билетов вы хотите приобрести?: '))
+    
+    sum = 0
+    for i in range(num_tickets):
+        age = int(input("Сколько лет %d посетителю?: " %(i+1)))
+            
+        if 0<=age<18:
+           pass
+        elif 18<=age<25:
+            sum += 990
+        elif age>=25:
+            sum += 1390
+        elif age<0:
+            raise AgeError("Неправильный возраст")
+
+except ValueError:
+    print("Ошибка ввода. Вероятно вы ввели не число или оставили поле пустым")
+    input()
+except AgeError:
+    print("Неправильный возраст")
+    input()
+
+
+discount = int(sum *0.1) if num_tickets > 3 else 0
+end_sum = sum-discount
+print("Итоговая сумма %d. Скидка %d" %(end_sum, discount))
