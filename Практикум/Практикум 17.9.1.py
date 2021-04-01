d70605025b504b2d01bf9c3d6136b1e5e392c4c0
Sergey <s.gluck94@gmail.com>
2021-04-01 23:41:22 +0300

    Исправил ошибку в алгоритме поиска

--- Практикум/Практикум 17.9.1.py
+++ Практикум/Практикум 17.9.1.py
@@ -9,12 +9,12 @@
 
 
 #--------Добавить проверку ввода!
-#numbers = list(map(int, input("Введите последовательность чисел через пробел \n").split())) #получаем список чисел сразу в int
-#user_num = int(input("Введите произвольное число"))
+numbers = list(map(int, input("Введите последовательность чисел через пробел \n").split())) #получаем список чисел сразу в int
+user_num = int(input("Введите произвольное число"))
 
 #--------Для отладки
-numbers = [1, 5, 10, 18, 7, 4, 8] 
-user_num = 10
+#numbers = [1, 5, 10, 18, 7, 4, 8] 
+#user_num = 6
 
 def BubbleSort(list):
     n=len(list)-1
@@ -28,16 +28,17 @@ def BinSearch(list,num,left,right):
     mid = len(list) // 2
     left = 0
     right = len(list) - 1
+         
+    if left > right:
+        return False
      
     while list[mid] != num and left <= right:
-        if num > list[mid]:
+        if num >= list[mid]:
             left = mid + 1
         else:
             right = mid - 1
         mid = (left + right) // 2
-     
-    if left > right:
-        return False
+
     else:
         return mid
 
@@ -45,4 +46,4 @@ sorted_numbers = BubbleSort(numbers)
 left = sorted_numbers[0]
 right = sorted_numbers[len(sorted_numbers)-1]
 print('Сортированный список: ', sorted_numbers)
-print('Позиция введенного числа (%d), отсчет идет с единицы: %d' % (user_num, BinSearch(sorted_numbers,user_num,left,right)+1))
+print('введенное число (%d) идет после числа на позиции %d, отсчет позиции начинается с единицы' % (user_num, BinSearch(sorted_numbers,user_num,left,right)+1))
